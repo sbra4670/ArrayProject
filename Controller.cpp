eae945b779cb2f20030f19890fdf5b9975657b0b
Sebastian <31518719+sbra4670@users.noreply.github.com>
2018-02-03 19:02:15 -0800

    Revert "Made a mistake and created a Runner.cpp file -_-"
    This reverts commit 797289f330549bc8fae8a7ef1e38eec25706170a.

--- Controller.cpp
+++ Controller.cpp
@@ -1,4 +1,5 @@
 #include "C:\Users\sea_b\Documents\cppProjects\ArrayProject\Controller.hpp"
+#include <iostream>
 
 using namespace std;
 
@@ -9,5 +10,23 @@ Controller :: Controller()
 
 void Controller :: start()
 {
+    int Bob[3] = 
+    {
+        1,2,3
+    };
 
+    printArray (Bob, 3);
 }
+
+   
+    void printArray (int IntArray[], int sizeOfArray)
+    {
+        for(int i = 0; i < sizeOfArray; i++)
+        {
+            cout << IntArray[i] << endl;
+        }   
+    }
+
+
+
+
